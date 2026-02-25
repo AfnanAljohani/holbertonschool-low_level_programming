b9d0a46b823fa604d50792353049925f09869d87
AfnanAljohani <AfnanAljohani@github.com>
2026-02-25 21:05:12 +0000

    Add task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers from 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
