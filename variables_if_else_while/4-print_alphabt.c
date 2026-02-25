f27cfb8d04b6c528ca11e7dbb1fea1bfb29797c4
AfnanAljohani <AfnanAljohani@github.com>
2026-02-25 21:02:43 +0000

    Add task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet excluding q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
