f77d48c0e32e7082763ce174ebe71a6c8a3cf33c
AfnanAljohani <AfnanAljohani@github.com>
2026-02-27 17:05:48 +0000

    Fix task 16 big numbers

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,28 @@
  */
 int main(void)
 {
-	double a, b, temp;
+	unsigned long a1, a2, b1, b2, t1, t2;
 	int i;
 
-	a = 1;
-	b = 2;
+	a1 = 1;
+	a2 = 0;
+	b1 = 2;
+	b2 = 0;
 	for (i = 0; i < 98; i++)
 	{
-		if (i == 97)
-			printf("%.0f\n", a);
+		if (a2 > 0)
+			printf("%lu%018lu", a2, a1);
 		else
-			printf("%.0f, ", a);
-		temp = a + b;
-		a = b;
-		b = temp;
+			printf("%lu", a1);
+		if (i < 97)
+			printf(", ");
+		t1 = a1 + b1;
+		t2 = a2 + b2 + (t1 < a1 ? 1 : 0);
+		a1 = b1;
+		a2 = b2;
+		b1 = t1;
+		b2 = t2;
 	}
+	printf("\n");
 	return (0);
 }
