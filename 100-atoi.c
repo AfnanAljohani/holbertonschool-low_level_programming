513bb86be4a1e48a0e5303a83bf59b744323f52e
AfnanAljohani <13258@holbertonstudents.com>
2026-03-05 21:45:45 +0000

    Fix task 10

---
+++ 100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the integer value of the string
+ */
+int _atoi(char *s)
+{
+	int result;
+	int neg;
+
+	result = 0;
+	neg = 0;
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			neg = !neg;
+		}
+		else if (*s == '+' || *s == ' ')
+		{
+			;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			while (*s >= '0' && *s <= '9')
+			{
+				result = result * 10 + (*s - '0');
+				s++;
+			}
+			if (neg)
+				return (-result);
+			return (result);
+		}
+		else
+		{
+			neg = 0;
+		}
+		s++;
+	}
+	return (0);
+}
