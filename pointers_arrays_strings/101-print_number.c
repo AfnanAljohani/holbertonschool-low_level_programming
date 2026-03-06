ee35fc7752ad88b34cd68236e2e2a0a39021430f
AfnanAljohani <AfnanAljohani@github.com>
2026-03-06 17:08:52 +0000

    Fix INT_MIN in print_number

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -11,16 +11,20 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		div = -1;
 	}
+	else
+		div = 1;
 
-	div = 1;
-	while (n / div >= 10)
+	while (n / div / 10 != 0)
 		div *= 10;
 
-	while (div >= 1)
+	while (div != 0)
 	{
-		_putchar('0' + n / div);
+		if (n < 0)
+			_putchar('0' - n / div);
+		else
+			_putchar('0' + n / div);
 		n %= div;
 		div /= 10;
 	}
