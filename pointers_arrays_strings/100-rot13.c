dafca9213167a2ee3f4379c54e89785541f9649e
AfnanAljohani <AfnanAljohani@github.com>
2026-03-06 16:56:08 +0000

    Add rot13 function

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to encode
+ *
+ * Return: pointer to the string
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char from[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char to[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (from[j] != '\0')
+		{
+			if (s[i] == from[j])
+				s[i] = to[j];
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
