9c39d35aa35c5f9f4ca082118456757db5b67353
AfnanAljohani <AfnanAljohani@github.com>
2026-03-09 22:09:36 +0000

    Fix 4-add to check non-digit characters

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,18 +10,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum, n;
+	int i, j, sum;
 
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		n = atoi(argv[i]);
-		if (n < 0)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += n;
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
