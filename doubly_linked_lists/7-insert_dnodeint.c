9d85ff64cf16044bae4b747f526fe2b35e027f2a
AfnanAljohani <13258@holbertonstudents.com>
2026-04-23 17:39:49 +0000

    Fix task 7: split function to comply with Betty 40 lines rule

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * create_node - creates a new dlistint_t node
+ * @n: integer value for the new node
+ *
+ * Return: pointer to the new node, or NULL if it failed
+ */
+dlistint_t *create_node(int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+	return (new);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: double pointer to the head of the list
  * @idx: index where the new node should be added (starting from 0)
@@ -16,43 +35,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-
 	if (idx == 0)
 	{
-		new = malloc(sizeof(dlistint_t));
+		new = create_node(n);
 		if (new == NULL)
 			return (NULL);
-		new->n = n;
-		new->prev = NULL;
 		new->next = *h;
 		if (*h != NULL)
 			(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
-
 	temp = *h;
 	while (temp != NULL && i < idx - 1)
 	{
 		temp = temp->next;
 		i++;
 	}
-
 	if (temp == NULL)
 		return (NULL);
-
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
-
-	new = malloc(sizeof(dlistint_t));
+	new = create_node(n);
 	if (new == NULL)
 		return (NULL);
-
-	new->n = n;
 	new->next = temp->next;
 	new->prev = temp;
 	temp->next->prev = new;
 	temp->next = new;
-
 	return (new);
 }
