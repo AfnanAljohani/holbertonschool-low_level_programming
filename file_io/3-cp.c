a1807d00ee3ca5e60ef672d2b55f82234f13ef33
AfnanAljohani <13258@holbertonstudents.com>
2026-04-25 12:26:30 +0000

    Fix Betty style in 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,11 +3,34 @@
 #include <stdio.h>
 
 /**
+ * open_files - opens source and destination files
+ * @av: argument vector
+ * @fd_from: pointer to source file descriptor
+ * @fd_to: pointer to destination file descriptor
+ */
+void open_files(char **av, int *fd_from, int *fd_to)
+{
+	*fd_from = open(av[1], O_RDONLY);
+	if (*fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	*fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (*fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(*fd_from);
+		exit(99);
+	}
+}
+
+/**
  * main - copies the content of a file to another file
  * @ac: argument count
  * @av: argument vector
  *
- * Return: 0 on success, exits with code on failure
+ * Return: 0 on success
  */
 int main(int ac, char **av)
 {
@@ -20,22 +43,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close(fd_from);
-		exit(99);
-	}
-
+	open_files(av, &fd_from, &fd_to);
 	while ((r = read(fd_from, buf, 1024)) > 0)
 	{
 		w = write(fd_to, buf, r);
@@ -47,7 +55,6 @@ int main(int ac, char **av)
 			exit(99);
 		}
 	}
-
 	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
@@ -55,18 +62,15 @@ int main(int ac, char **av)
 		close(fd_to);
 		exit(98);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	return (0);
 }
